20235a2d714931625dcd205197fca2eb84be286c
Mrinal <mrinaltewary@gmail.com>
2024-02-08 22:21:50 +0530

    removed seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,13 @@ export default function Home() {
     <>
       {/* <p>Hello</p> */}
       {/* <Hello /> */}
-      <SEO
+      {/* <SEO
         title="Donate for Pnina Basov"
         description="Pnina Basov, 18 year old girl in Canada -from Haifa Israel,
         Playing professional hockey in Canada and wants continue education in USA but the thing is that she is running our of money and in the spring if she will not find it she will go to army in Israel.
         leading to of sport career"
         image="https://files.eliteprospects.com/layout/players/pnina_basov_isr.jpg"
-      />
+      /> */}
       <Header />
       <Layout />
       <Footer />
